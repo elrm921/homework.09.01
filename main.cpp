ee30a5b73c4bbd07ab2e56ebbba08cd30455dce5
elrm921 <pocketrocket03@gmail.com>
2022-07-08 15:46:25 +0300

    updated

--- main.cpp
+++ main.cpp
@@ -12,20 +12,25 @@ public:
 		numerator_ = numerator;
 		denominator_ = denominator;
 	}
-    double toDouble() {
-        return numerator_ / denominator_;
+    int find_gcd(int a, int b) {
+        if (a > b) return find_gcd(a-b, b);
+        else if (a < b) return find_gcd(a, b-a);
+        else return a;
     }
     bool operator ==(Fraction other) {
-        return toDouble() == other.toDouble();
+        int cd = denominator_ * other.denominator_;
+        return ((numerator_ * cd / denominator_) == (other.numerator_ * cd / other.denominator_));
+    }
+    bool operator <(Fraction other) {
+        int cd = denominator_ * other.denominator_;
+        return ((numerator_ * cd / denominator_) < (other.numerator_ * cd / other.denominator_));
+    }
+    bool operator >(Fraction other) {
+        int cd = denominator_ * other.denominator_;
+        return ((numerator_ * cd / denominator_) > (other.numerator_ * cd / other.denominator_));
     }
     bool operator !=(Fraction other) {
         return !(*this == other);
-    }
-    bool operator <(Fraction other) {
-        return toDouble() < other.toDouble();
-    }
-    bool operator >(Fraction other) {
-        return toDouble() > other.toDouble();
     }
     bool operator <=(Fraction other) {
         return !(*this > other);
